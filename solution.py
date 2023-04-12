de67ea13746bb66f251eafa38d84609d1ef8685c
grungemalevich <118844391+grungemalevich@users.noreply.github.com>
2023-04-13 00:07:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
+from hyppo.ksample import Energy, MMD
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 487727948 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    p_value = MMD(compute_kernel="rbf", gamma=1).test(x, y)[1]
+
+    # Если p-уровень значимости меньше заданного уровня значимости, отклоняем гипотезу однородности выборок
+    return p_value < 0.01
